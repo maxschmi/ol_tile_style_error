719afa83792013f8c117a6594aab5f4558f9cdd3
Max Schmit <46047581+maxschmi@users.noreply.github.com>
2024-07-17 13:00:04 +0000

    add hover for pixel value

--- main.js
+++ main.js
@@ -1,37 +1,44 @@
-import './style.css';
-import { Map, View } from 'ol';
-import TileLayer from 'ol/layer/WebGLTile.js';
+import "./style.css";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/WebGLTile.js";
 import { GeoTIFF } from "ol/source";
-import proj4 from 'proj4';
-import { register } from 'ol/proj/proj4';
+import proj4 from "proj4";
+import { register } from "ol/proj/proj4";
 
-proj4.defs("EPSG:25832", "+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs");
+// define projection
+proj4.defs(
+  "EPSG:25832",
+  "+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs",
+);
 register(proj4);
 
+// create style definition
 const style = {
-    color: [
-      "case",
-      ["!=", ["band", 2], 0],
-      ["color", 147, 68, 144, 1],
-      ["color", 0, 0, 0, 0]
-    ]
-  }
+  color: [
+    "case",
+    ["!=", ["band", 2], 0],
+    ["color", 147, 68, 144, 1],
+    ["color", 0, 0, 0, 0],
+  ],
+};
+
+// create layer
 const tifLayer = new TileLayer({
   source: new GeoTIFF({
     sources: [
       {
         url: "test.tif",
-        bands: [1]
-      }
+        bands: [1],
+      },
     ],
     sourceOptions: {
       allowFullFile: true,
     },
     interpolate: false,
-    normalize: false
+    normalize: false,
   }),
   visible: true,
-  style: style
+  style: style,
 });
 
 // turn this line on and off
@@ -42,19 +49,20 @@ tifLayer.setStyle(style);
 // create map
 let tifView = await tifLayer.getSource().getView();
 const map = new Map({
-  target: 'map',
-  layers: [
-    tifLayer
-  ],
+  target: "map",
+  layers: [tifLayer],
   view: new View({
     projection: tifView.projection,
-    center: tifView.center,
-    zoom: 14
-  })
+    center: [445190, 5296943],
+    zoom: 16,
+  }),
 });
+window.map = map;
 
+// hover info
+const hoverDiv = document.getElementById("hover-value");
 map.on("pointermove", (e) => {
   const pixel = map.getEventPixel(e.originalEvent);
   const value = tifLayer.getData(pixel);
-  console.log(value);
+  hoverDiv.innerHTML = JSON.stringify(value);
 });
